1668b710c766526a58ccf6433834e3404e1b6480
JHON EDISON SANDOVAL BARRETO <JHONSAN89@hotmail.com>
2018-10-20 11:44:08 -0500

    inatanciando BD

---
+++ redsocial.api/Controllers/PublicacionController.cs
@@ -0,0 +1,23 @@
+﻿using redsocial.api.Models;
+using Redsocial.dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace redsocial.api.Controllers
+{
+    public class PublicacionController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Publicacion> Get()
+        {
+            using (PublicacionContext context = new PublicacionContext())
+            {
+                return context.Publicaciones.ToList();
+            }
+        }
+    }
+}
