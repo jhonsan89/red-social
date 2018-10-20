746f8725231822932d5dd1ef8d79750143c72eef
JHON EDISON SANDOVAL BARRETO <JHONSAN89@hotmail.com>
2018-10-20 11:16:53 -0500

    cadena de coneccion, contextDB,Carpeta dominio

--- Redsocial.dominio/Publicacion.cs
+++ Redsocial.dominio/Publicacion.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Redsocial.dominio
 {
+
+    [Table("Publicacion")]
     public class Publicacion
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Usuario { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Descripcion { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int MeGusta { get; set; }
+        public int MeDisgusta { get; set; }
+        public int VecesCompartido { get; set; }
+        public bool EsPrivada { get; set; }
     }
 }
---
+++ redsocial.api/Models/PublicacionContext.cs
@@ -0,0 +1,19 @@
+﻿using Redsocial.dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace redsocial.api.Models
+{
+    public class PublicacionContext : DbContext
+    {
+        public PublicacionContext() : base("PublicacionConnection")
+        {
+
+        }
+
+        public DbSet<Publicacion> Publicaciones { get; set; }
+    }
+}
